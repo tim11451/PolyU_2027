8270b2bf70089d86919d625a9fdb6f7a273ccada
tim11451 <156736489+tim11451@users.noreply.github.com>
2025-01-22 22:33:44 +0800

    Add files via upload

---
+++ COMP2021/hk/edu/polyu/comp/comp2021/cvfs/Application.java
@@ -0,0 +1,10 @@
+package hk.edu.polyu.comp.comp2021.cvfs;
+
+import hk.edu.polyu.comp.comp2021.cvfs.model.CVFS;
+
+public class Application {
+    public static void main(String[] args) {
+        CVFS cvfs = new CVFS();
+        cvfs.CLI();
+    }
+}
